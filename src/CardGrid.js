c7bb2ab8a872864c0abff6ef72105eb15743f93b
maniart <mani.art@gmail.com>
2020-08-20 13:31:30 -0400

    wip

--- src/CardGrid.js
+++ src/CardGrid.js
@@ -1,14 +1,22 @@
 import React, { useState, useCallback } from 'react';
-import Card from './Card'; 
+import Card from './Card';
 
 function CardGrid({ nodes }) {
   return (
-    <section className="card-grid flex flex-row  flex-wrap">
-      {
-        nodes.map((node, idx) => {
-          return (<Card key={idx} details={node} />)
-        })
-      }
+    <section className="card-grid relative flex flex-column">
+      <header className={`ph2 fixed flex items-center flex-row filters-header relative ${false ? 'minimized' : 'expanded'}`} onClick={() => { console.log('header click') }}>
+        <div className="absolute fw7 f2 close-icon">
+          +
+        </div>
+      </header>
+      <main class="flex flex-row flex-wrap">
+        {
+          nodes.map((node, idx) => {
+            return (<Card key={idx} details={node} />)
+          })
+        }
+      </main>
+
     </section>
   )
 }
