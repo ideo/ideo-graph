4ddcbc5b9ece7728bbcc7e25e1d924e4048c50a7
maniart <mani.art@gmail.com>
2020-08-21 23:27:06 -0400

    wip

--- src/CardGrid.js
+++ src/CardGrid.js
@@ -2,6 +2,23 @@ import React, { useState, useCallback } from 'react';
 import Card from './Card';
 
 function CardGrid({ nodes, handleCloseClick }) {
+  const sorted = nodes.sort((one, two) => {
+    if (one.fullName > two.fullName) {
+      return 1;
+    }
+    if (two.fullName > one.fullName) {
+        return -1;
+    }
+    return 0;
+  })
+  // put mitch last
+  if (!!sorted.find(node => node.fullName === 'Mitch Sinclair')) {
+    const mitch = sorted.find(node => node.fullName === 'Mitch Sinclair')
+    const index = sorted.indexOf(mitch);
+    const [node] = sorted.splice(index,1)
+    sorted.push(node)
+  }
+
   return (
     <section className="card-grid pb2 relative flex flex-column">
       <header className={`ph2 fixed flex items-center flex-row filters-header relative ${false ? 'minimized' : 'expanded'}`}>
@@ -11,20 +28,7 @@ function CardGrid({ nodes, handleCloseClick }) {
       </header>
       <main className="flex flex-row flex-wrap">
         {
-          nodes.sort((one, two) => {
-            const firstName1 = one.firstName
-            const firstName2 = two.firstName
-            // console.log('_____',    two.fullName, one.fullName)
-            if (one.fullName > two.fullName) {
-              return 1;
-            }
-            if (two.fullName > one.fullName) {
-                return -1;
-            }
-            return 0;
-            // return one.fullName - two.fullName
-
-          }).map((node, idx) => {
+         sorted.map((node, idx) => {
             return (<Card key={idx} details={node} />)
           })
         }
